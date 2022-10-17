492112dfa7c215477b3055d45a155f2c4dbcca9b
iykuetboo <iykuetboo@gmail.com>
2022-10-18 00:48:41 +0900

    test model admin

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,14 @@
+import email
+from email.policy import default
+from importlib.metadata import requires
 from django.db import models
 
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+    name = models.TextField("name",default="hoge")
+
+# class User(models.Model):
+#     name = models.TextField(requires=True)
+#     email = models.TextField()
+
