8cceda1a3b73701b42bd912c6b16ed0f9ea2362d
iykuetboo <iykuetboo@gmail.com>
2022-10-19 03:06:58 +0900

    test change

--- aiconapi/tests.py
+++ aiconapi/tests.py
@@ -6,7 +6,7 @@ url = 'https://aicon-maker-backend.herokuapp.com/aiconapi/check_result_nodb'
 # url = 'http://localhost:8000/aiconapi/check_result_nodb'
 sess = requests.session()
 
-print(sess.get(url))
+# print(sess.get(url))
 
 # csrftoken = sess.cookies['csrftoken']
 # print(csrftoken)
@@ -16,7 +16,7 @@ print(sess.get(url))
 headers = {'Content-type': 'application/json'}
 
 # 送信データ
-prm = {'id': sys.argv[-1], 'user': 'nima'}
+prm = {'id': 'test_10'}
 
 # JSON変換
 params = json.dumps(prm)
@@ -24,7 +24,24 @@ params = json.dumps(prm)
 # POST送信
 res = sess.post(url, data=params, headers=headers)
 # 戻り値を表示
-print(res)
+print('10人待ちのはず')
 if res.status_code == 200:
     print(json.loads(res.text))
-    
+else:
+    print(res)
+
+
+# 送信データ
+prm = {'id': 'finishshshshsh'}
+
+# JSON変換
+params = json.dumps(prm)
+
+# POST送信
+res = sess.post(url, data=params, headers=headers)
+# 戻り値を表示
+print('sampleのアイコンURLが帰るはず')
+if res.status_code == 200:
+    print(json.loads(res.text))
+else:
+    print(res)
